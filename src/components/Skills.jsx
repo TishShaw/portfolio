ac9c70d776f78f8c6c641e2b3635ee8f232f4ba2
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-03-26 20:42:16 -0400

    Add skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import HTML from '../assets/icons/html.png';
+import JAVASCRIPT from '../assets/icons/javascript.png';
+import CSS from '../assets/icons/css.png';
+import REACT from '../assets/icons/react-original.svg';
+import TYPESCRIPT from '../assets/icons/typescript-plain.svg';
+import PYTHON from '../assets/icons/python-original.svg';
+import MONGO from '../assets/icons/mongo.png';
+import AWS from '../assets/icons/aws.png';
+
+
+function Skills(props) {
+    return (
+        <div name='skills' className='w-full h-screen items-center text-gray-400 my-20'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600 animate-fade-in-down'>Skills</p>
+                    <p className='py-4'>Technologies i've worked with</p>
+                </div>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={HTML} alt='HTML icon' />
+                        <p className='my-4'>HTML</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={JAVASCRIPT} alt='HTML icon' />
+                        <p className='my-4'>JAVASCRIPT</p>
+                    </div>
+                    <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={CSS} alt='HTML icon' />
+                        <p className='my-4'>CSS</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={REACT} alt='HTML icon' />
+                        <p className='my-4'>REACT</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={TYPESCRIPT} alt='HTML icon' />
+                        <p className='my-4'>TYPESCRIPT</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={PYTHON} alt='HTML icon' />
+                        <p className='my-4'>PYTHON</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={MONGO} alt='HTML icon' />
+                        <p className='my-4'>MongoDB</p>
+                    </div>
+                     <div className='shadow-md hover:scale-110 duration-500'>
+                        <img className='w-20 mx-auto' src={AWS} alt='HTML icon' />
+                        <p className='my-4'>AWS</p>
+                    </div>
+                     
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
